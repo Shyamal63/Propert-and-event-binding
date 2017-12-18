ddaca9f02e5e2eaac8a1adcf837aff0c7e1d65a1
shyamal63 <gayen.shyamal@gmail.com>
2017-12-18 18:52:02 +0530

    property and event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+serverElements=[{type: 'server', name: 'Testserver', content: 'just a test!'}];
+onServerAdded(serverData : {serverName: string; serverContent: string}) {
+   this.serverElements.push({
+   type:'server',
+    name:serverData.serverName,
+    content:serverData.serverContent
+
+
+   });
+
+ }
+ onBlueprintAdded(blueprintData: {serverName: string,serverContent: string}){
+   this.serverElements.push({
+     type: 'blueprint',
+     name: blueprintData.serverName,
+     content: blueprintData.serverContent
+   });
+ }
 }
+ 
+
